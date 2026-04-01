43ceed963afd1a905783fe598bdeea4889f432c5
getoconas <getoconas@gmail.com>
2026-03-31 21:01:47 -0300

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'calculator', component: CalculatorComponent },
+  { path: '**', pathMatch:'full', redirectTo:'calculator' }
+];
